1eea917054344172c2e6ea41f07d996fbfbcea35
051-lab <darius.vinson@tech901.org>
2026-08-09 00:34:24 -0500

    test: reproduce dropped article markdown entity

---
+++ tests/test_article_markdown_entities.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from x_media_downloader.article_html import to_html
+
+
+def test_atomic_markdown_fence_renders_code_block() -> None:
+    article = {
+        "content_state": {
+            "blocks": [
+                {
+                    "type": "atomic",
+                    "text": " ",
+                    "entityRanges": [{"key": "markdown", "offset": 0, "length": 1}],
+                }
+            ],
+            "entityMap": [
+                {
+                    "key": "markdown",
+                    "value": {
+                        "type": "MARKDOWN",
+                        "data": {
+                            "markdown": (
+                                "```bash\n"
+                                "mkdir ~/projects/multifactor-alpha\n"
+                                "cd ~/projects/multifactor-alpha\n"
+                                "```"
+                            )
+                        },
+                    },
+                }
+            ],
+        },
+        "media_entities": [],
+    }
+
+    assert "".join(to_html(article)) == (
+        "<pre><code>"
+        "mkdir ~/projects/multifactor-alpha\n"
+        "cd ~/projects/multifactor-alpha"
+        "</code></pre>\n"
+    )
